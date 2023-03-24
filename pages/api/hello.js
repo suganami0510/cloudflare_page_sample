b6eefc7468eb84f052dd02c37b2bc68b83335f95
Ryosuke Suganami <*****@users.noreply.github.com>
2023-03-24 23:26:24 +0900

    kv test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,8 @@
 export const config = {
   runtime: 'edge',
-};
+}
 
 export default async function (req) {
-  return Response.json({ name: 'John Doe' });
+  const kvvalue = await process.env.KV.get('test')
+  return new Response(kvvalue)
 }
